61b20d38e5e4f7d071a4a316c80d416b2a27f1cf
brandonrvice@gmail.com <38141231+brandonrvice@users.noreply.github.com>
2019-11-12 17:06:02 -0800

    Updated service calls to use variable for server.

--- src/services/ArticleService.js
+++ src/services/ArticleService.js
@@ -1,30 +1,32 @@
 import axios from "axios";
 
 const articleService = {};
+const httpServer = "35.166.8.20";
+// const httpServer = "localhost:8000";
 
 articleService.getArticlesAsync = async () => {
-  let res = await axios.get("http://localhost:8000/api/articles");
+  let res = await axios.get(`http://${httpServer}/api/articles`);
   return res.data;
 };
 
 articleService.getArticlesFromIdsAsync = async ids => {
-  let res = await axios.post("http://localhost:8000/api/articles", ids);
+  let res = await axios.post(`http://${httpServer}/api/articles`, ids);
   return res.data;
 };
 
 articleService.upvote = async id => {
-  let res = await axios.post(`http://localhost:8000/api/article/${id}/upvote`);
+  let res = await axios.post(`http://${httpServer}/api/article/${id}/upvote`);
   return res.data;
 };
 
 articleService.addComment = async (articleId, comment) => {
-  let res = await axios.post(`http://localhost:8000/api/article/${articleId}/comment`, comment);
+  let res = await axios.post(`http://${httpServer}/api/article/${articleId}/comment`, comment);
   return res.data;
 };
 
 articleService.getArticleAsync = async id => {
   try {
-    let res = await axios.get(`http://localhost:8000/api/article/${id}`);
+    let res = await axios.get(`http://${httpServer}/api/article/${id}`);
     return res.data;
   } catch (err) {
     console.log(err);
